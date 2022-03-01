8a1de767e17d5cfc334ebd9b5ee0b6a6a10d2293
Sergey Lukashev <lukashev.s@ya.ru>
2022-03-01 20:23:32 +0300

    added retry HTTP 429 logic

--- src/Submissions/SubmissionsClient.php
+++ src/Submissions/SubmissionsClient.php
@@ -24,8 +24,7 @@ class SubmissionsClient
     private function getAccessToken()
     {
         return $this->_tokenProvider->GetSubmissionsTokenAsync()->then(
-            function($token)
-            {
+            function ($token) {
                 return $token->Token;
             }
         );
@@ -34,44 +33,59 @@ class SubmissionsClient
     public function issueRequest(string $method, $uri, $jsonBody = null, array $addHeaders = [])
     {
         return $this->getAccessToken()->then(function ($token)
-            use ($method, $uri, $jsonBody, $addHeaders)
-            {
-                $authHeader = "Bearer $token";
-                // if jsonBody is a string add body and Content-Type
-                // otherwise add json
-                $options = array_merge([
-                    'headers' => array_merge(
-                        ['Authorization' => $authHeader],
-                        is_string($jsonBody) ? ['Content-Type' => 'application/json'] : [],
-                        $addHeaders,
-                    ),
-                ], is_null($jsonBody) ? [] : (is_string($jsonBody) ? ['body' => $jsonBody] : ['json' => $jsonBody]));
+        use ($method, $uri, $jsonBody, $addHeaders) {
+            $authHeader = "Bearer $token";
+            // if jsonBody is a string add body and Content-Type
+            // otherwise add json
+            $options = array_merge([
+                'headers' => array_merge(
+                    ['Authorization' => $authHeader],
+                    is_string($jsonBody) ? ['Content-Type' => 'application/json'] : [],
+                    $addHeaders,
+                ),
+            ], is_null($jsonBody) ? [] : (is_string($jsonBody) ? ['body' => $jsonBody] : ['json' => $jsonBody]));
 
-                return $this->_httpClient->requestAsync($method, $uri, $options);
-            });
+            return $this->_httpClient->requestAsync($method, $uri, $options);
+        });
     }
 
-    private function responseCallback(Response $resp, string $callDescription, ?string $responseClass = null)
+    private function responseCallback(Response $resp, string $callDescription, ?string $responseClass = null, callable $retryFunction)
     {
-        //($callDescription);
+        //print($callDescription . "\n");
         $status = $resp->getStatusCode();
         $body = (string) $resp->getBody();
-        if($status >= 200 && $status < 300)
-        {
-            if(!is_null($responseClass))
+        if ($status >= 200 && $status < 300) {
+            if (!is_null($responseClass))
                 return Serializer::Get()->deserialize($body, $responseClass, 'json');
             else
                 return true;
-        }
-        else
-        {
-            throw new StoreServicesHttpResponseException("{$callDescription} failed [{$status}] {$body}" , $resp);
+        } else if ($status === 429) {
+            if (!is_null($retryFunction)) {
+                try {
+                    $message = json_decode($body, true)['message'];
+                    if (preg_match('/Try again in (\d+) seconds/', $message, $matches, PREG_OFFSET_CAPTURE)) {
+                        $timeout = intval($matches[1][0]) + 1;
+                        print("Rate limit is exceeded. Retrying in $timeout seconds.");
+                        sleep($timeout);
+                        return $retryFunction();
+                    }
+                } catch (\Exception $e) {
+                    print("failed to retry request: " . $e->getMessage() . "\n");
+                    throw new StoreServicesHttpResponseException("{$callDescription} failed [{$status}] {$body}", $resp);
+                }
+            } else {
+                throw new StoreServicesHttpResponseException("{$callDescription} failed [{$status}] {$body}", $resp);
+            }
+        } else {
+            throw new StoreServicesHttpResponseException("{$callDescription} failed [{$status}] {$body}", $resp);
         }
     }
 
     private function args2str($args)
     {
-        return '('. join(', ', array_map(function($a){ return (string) $a; }, $args)) . ')';
+        return '(' . join(', ', array_map(function ($a) {
+            return (string) $a;
+        }, $args)) . ')';
     }
 
     private function getAbsoluteUri(string $relative)
@@ -81,109 +95,172 @@ class SubmissionsClient
 
     public function getApplicationAddOnsByLink(string $link)
     {
-        $callDescription = __FUNCTION__ . $this->args2str(func_get_args());
+        $args = func_get_args();
+        $callDescription = __FUNCTION__ . $this->args2str($args);
+        $funcName = __FUNCTION__;
+        $thisFunction = function () use ($args, $funcName) {
+            return call_user_func_array([__CLASS__, $funcName], $args);
+        };
+
         return $this->issueRequest('GET', $this->getAbsoluteUri($link))
-        ->then(function($resp) use($callDescription){
-            return $this->responseCallback($resp, $callDescription, AppAddOnsResponse::class);
-        });
+            ->then(function ($resp) use ($callDescription, $thisFunction) {
+                return $this->responseCallback($resp, $callDescription, AppAddOnsResponse::class, $thisFunction);
+            });
     }
 
     public function getApplicationAddOns(string $applicationId, ?int $top = null, ?int $skip = null)
     {
         $params = [];
-        if(!is_null($top))
+        if (!is_null($top))
             $params['top'] = $top;
-        if(!is_null($skip))
+        if (!is_null($skip))
             $params['skip'] = $skip;
 
         $link = "https://manage.devcenter.microsoft.com/v1.0/my/applications/{$applicationId}/listinappproducts";
-        if(count($params))
+        if (count($params))
             $link = $link . '?' . http_build_query($params);
 
-        $callDescription = __FUNCTION__ . $this->args2str(func_get_args());
+        $args = func_get_args();
+        $callDescription = __FUNCTION__ . $this->args2str($args);
+        $funcName = __FUNCTION__;
+        $thisFunction = function () use ($args, $funcName) {
+            return call_user_func_array([__CLASS__, $funcName], $args);
+        };
+
         return $this->issueRequest('GET', $link)
-        ->then(function($resp) use($callDescription){
-            return $this->responseCallback($resp, $callDescription, AppAddOnsResponse::class);
-        });
+            ->then(function ($resp) use ($callDescription, $thisFunction) {
+                return $this->responseCallback($resp, $callDescription, AppAddOnsResponse::class, $thisFunction);
+            });
     }
 
     public function createAddOn(string $appId, string $productId, string $productType)
     {
-        $callDescription = __FUNCTION__ . $this->args2str(func_get_args());
-        return $this->issueRequest('POST', 'https://manage.devcenter.microsoft.com/v1.0/my/inappproducts',
-        [
-            'applicationIds' => [$appId],
-            'productId' => $productId,
-            'productType' => $productType,
-        ])
-        ->then(function($resp) use($callDescription){
-            return $this->responseCallback($resp, $callDescription, AddOnResource::class);
-        });
+        $args = func_get_args();
+        $callDescription = __FUNCTION__ . $this->args2str($args);
+        $funcName = __FUNCTION__;
+        $thisFunction = function () use ($args, $funcName) {
+            return call_user_func_array([__CLASS__, $funcName], $args);
+        };
+
+        return $this->issueRequest(
+            'POST',
+            'https://manage.devcenter.microsoft.com/v1.0/my/inappproducts',
+            [
+                'applicationIds' => [$appId],
+                'productId' => $productId,
+                'productType' => $productType,
+            ]
+        )
+            ->then(function ($resp) use ($callDescription, $thisFunction) {
+                return $this->responseCallback($resp, $callDescription, AddOnResource::class, $thisFunction);
+            });
     }
 
     public function getAddOn(string $productId)
     {
-        $callDescription = __FUNCTION__ . $this->args2str(func_get_args());
+        $args = func_get_args();
+        $callDescription = __FUNCTION__ . $this->args2str($args);
+        $funcName = __FUNCTION__;
+        $thisFunction = function () use ($args, $funcName) {
+            return call_user_func_array([__CLASS__, $funcName], $args);
+        };
+
         return $this->issueRequest('GET', "https://manage.devcenter.microsoft.com/v1.0/my/inappproducts/{$productId}")
-        ->then(function($resp) use($callDescription){
-            return $this->responseCallback($resp, $callDescription, AddOnResource::class);
-        });
+            ->then(function ($resp) use ($callDescription, $thisFunction) {
+                return $this->responseCallback($resp, $callDescription, AddOnResource::class, $thisFunction);
+            });
     }
 
     public function deleteAddOn($productId)
     {
-        $callDescription = __FUNCTION__ . $this->args2str(func_get_args());
+        $args = func_get_args();
+        $callDescription = __FUNCTION__ . $this->args2str($args);
+        $funcName = __FUNCTION__;
+        $thisFunction = function () use ($args, $funcName) {
+            return call_user_func_array([__CLASS__, $funcName], $args);
+        };
+
         return $this->issueRequest('DELETE', "https://manage.devcenter.microsoft.com/v1.0/my/inappproducts/{$productId}")
-        ->then(function($resp) use($callDescription){
-            return $this->responseCallback($resp, $callDescription);
-        });
+            ->then(function ($resp) use ($callDescription, $thisFunction) {
+                return $this->responseCallback($resp, $callDescription, null, $thisFunction);
+            });
     }
 
     public function createAddOnSubmission(string $productId)
     {
-        $callDescription = __FUNCTION__ . $this->args2str(func_get_args());
+        $args = func_get_args();
+        $callDescription = __FUNCTION__ . $this->args2str($args);
+        $funcName = __FUNCTION__;
+        $thisFunction = function () use ($args, $funcName) {
+            return call_user_func_array([__CLASS__, $funcName], $args);
+        };
+
         return $this->issueRequest('POST', "https://manage.devcenter.microsoft.com/v1.0/my/inappproducts/{$productId}/submissions")
-        ->then(function($resp) use($callDescription){
-            return $this->responseCallback($resp, $callDescription, AddOnSubmissionResource::class);
-        });
+            ->then(function ($resp) use ($callDescription, $thisFunction) {
+                return $this->responseCallback($resp, $callDescription, AddOnSubmissionResource::class, $thisFunction);
+            });
     }
 
     public function getAddOnSubmission(string $productId, string $submissionId)
     {
-        $callDescription = __FUNCTION__ . $this->args2str(func_get_args());
+        $args = func_get_args();
+        $callDescription = __FUNCTION__ . $this->args2str($args);
+        $funcName = __FUNCTION__;
+        $thisFunction = function () use ($args, $funcName) {
+            return call_user_func_array([__CLASS__, $funcName], $args);
+        };
+
         return $this->issueRequest('GET', "https://manage.devcenter.microsoft.com/v1.0/my/inappproducts/{$productId}/submissions/{$submissionId}")
-        ->then(function($resp) use($callDescription){
-            return $this->responseCallback($resp, $callDescription, AddOnSubmissionResource::class);
-        });
+            ->then(function ($resp) use ($callDescription, $thisFunction) {
+                return $this->responseCallback($resp, $callDescription, AddOnSubmissionResource::class, $thisFunction);
+            });
     }
 
     public function updateAddOnSubmission(string $productId, string $submissionId, UpdateAddOnSubmissionResource $data)
     {
-        $callDescription = __FUNCTION__ . $this->args2str(func_get_args());
+        $args = func_get_args();
+        $callDescription = __FUNCTION__ . $this->args2str($args);
+        $funcName = __FUNCTION__;
+        $thisFunction = function () use ($args, $funcName) {
+            return call_user_func_array([__CLASS__, $funcName], $args);
+        };
+
         return $this->issueRequest(
             'PUT',
             "https://manage.devcenter.microsoft.com/v1.0/my/inappproducts/{$productId}/submissions/{$submissionId}",
             (string) $data
-        )->then(function($resp) use($callDescription){
-            return $this->responseCallback($resp, $callDescription, AddOnSubmissionResource::class);
+        )->then(function ($resp) use ($callDescription, $thisFunction) {
+            return $this->responseCallback($resp, $callDescription, AddOnSubmissionResource::class, $thisFunction);
         });
     }
 
     public function deleteAddOnSubmission(string $productId, string $submissionId)
     {
-        $callDescription = __FUNCTION__ . $this->args2str(func_get_args());
+        $args = func_get_args();
+        $callDescription = __FUNCTION__ . $this->args2str($args);
+        $funcName = __FUNCTION__;
+        $thisFunction = function () use ($args, $funcName) {
+            return call_user_func_array([__CLASS__, $funcName], $args);
+        };
+
         return $this->issueRequest('DELETE', "https://manage.devcenter.microsoft.com/v1.0/my/inappproducts/{$productId}/submissions/{$submissionId}")
-        ->then(function($resp) use($callDescription){
-            return $this->responseCallback($resp, $callDescription);
-        });
+            ->then(function ($resp) use ($callDescription, $thisFunction) {
+                return $this->responseCallback($resp, $callDescription, null, $thisFunction);
+            });
     }
 
     public function commitAddOnSubmission(string $productId, string $submissionId)
     {
-        $callDescription = __FUNCTION__ . $this->args2str(func_get_args());
+        $args = func_get_args();
+        $callDescription = __FUNCTION__ . $this->args2str($args);
+        $funcName = __FUNCTION__;
+        $thisFunction = function () use ($args, $funcName) {
+            return call_user_func_array([__CLASS__, $funcName], $args);
+        };
+
         return $this->issueRequest('POST', "https://manage.devcenter.microsoft.com/v1.0/my/inappproducts/{$productId}/submissions/{$submissionId}/commit")
-        ->then(function($resp) use($callDescription){
-            return $this->responseCallback($resp, $callDescription, CommitResponse::class);
-        });
+            ->then(function ($resp) use ($callDescription, $thisFunction) {
+                return $this->responseCallback($resp, $callDescription, CommitResponse::class, $thisFunction);
+            });
     }
 }
